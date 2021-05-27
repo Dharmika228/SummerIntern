bc094e431b0d00c8ec86155ae134143d814408e5
Dharmika228 <61747322+Dharmika228@users.noreply.github.com>
2021-05-27 22:54:52 +0530

    Add files via upload

--- model_trained.py
+++ model_trained.py
@@ -30,3 +30,18 @@ import joblib
 joblib.dump(model,'SalaryData.pk1')
 
 print("model has been saved successfully")
+
+// saving the model
+
+print(""" 
+	to save the model:
+	  Y for yes
+	  N for no
+	""")
+c = input("enter your choice")
+if "Y" in c:
+    import joblib
+    m1 = joblib.load('SalaryData.pk1')
+    print("successfully built")
+if "N" in c:
+    break  
